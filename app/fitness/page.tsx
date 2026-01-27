0c406ce28831003d6d2d946d8e9e9b9d0d49bda5
Prince <princemehta516@gmail.com>
2026-01-28 00:08:02 +0530

    upload video

---
+++ app/fitness/page.tsx
@@ -0,0 +1,11 @@
+export default function VideoPlayer() {
+    return (
+      <video
+        src="Fundamentals1.mp4"
+        controls
+        width="1000"
+        preload="metadata"
+      />
+    );
+  }
+  
