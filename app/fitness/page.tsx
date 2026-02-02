9e3eb2c528520c11c0737c5a87b7366fc1de4aff
Prince <90469232+Prince-0000@users.noreply.github.com>
2026-02-02 10:20:52 +0530

    Update page.tsx

--- app/fitness/page.tsx
+++ app/fitness/page.tsx
@@ -1,11 +1,8 @@
-// export default function VideoPlayer() {
-//     return (
-//       <video
-//         src="Fundamentals1.mp4"
-//         controls
-//         width="1000"
-//         preload="metadata"
-//       />
-//     );
-//   }
+export default function VideoPlayer() {
+    return (
+      <>
+       Fitness Page
+      </>
+    );
+   }
   
